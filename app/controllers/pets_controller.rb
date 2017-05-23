5522bfb0a06affdbf447be75b2e63e643f46048f
Alex Benoit <alexander.benoit@outlook.com>
2017-05-23 18:31:34 +0100

    done the new and create

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,11 +8,14 @@ class PetsController < ApplicationController
   end
 
   def new
-
+    @pet = Pet.new
   end
 
   def create
-
+    pet_params = params.require(:pet).permit(:name, :category, :found)
+    new_pet = Pet.new(pet_params)
+    new_pet.save
+    redirect_to pets_path
   end
 
   def edit
