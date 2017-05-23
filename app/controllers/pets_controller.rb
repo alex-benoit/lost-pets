94677e0f12c9858548ed295e7aa07c8f6ea783ce
Alex Benoit <alexander.benoit@outlook.com>
2017-05-23 18:46:50 +0100

    done all the routes

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,10 +1,11 @@
 class PetsController < ApplicationController
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
+
   def index
     @pets = Pet.all
   end
 
   def show
-    @pet = Pet.find(params[:id])
   end
 
   def new
@@ -12,21 +13,39 @@ class PetsController < ApplicationController
   end
 
   def create
-    pet_params = params.require(:pet).permit(:name, :category, :found)
     new_pet = Pet.new(pet_params)
     new_pet.save
     redirect_to pets_path
   end
 
   def edit
-
   end
 
   def update
-
+    @pet.update(pet_params)
+  redirect_to pet_path(@pet)
   end
 
   def destroy
+    @pet.destroy
+    redirect_to pets_path
+  end
 
+  private
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(:name, :category, :found)
   end
 end
+
+
+
+
+
+
+
+
